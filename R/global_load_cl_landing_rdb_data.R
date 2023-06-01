c4a013834adffc5aca2379fab30179f7cb6f9348
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2023-06-01 16:52:24 +0200

    Implemnted new way of importing RDB data

--- R/global_load_cl_landing_rdb_data.R
+++ R/global_load_cl_landing_rdb_data.R
@@ -3,57 +3,98 @@
 #' @description Process for for load the data from CL landing RDB data file.
 #' @param input_path_cl_landing_rdbes_data {\link[base]{character}} expected. Input CL landing RDB data file path (.csv format expected).
 #' @return A tibble.
-#' @importFrom readr read_csv
+#' @importFrom readr read_csv dplyr group_by summarise rename tidyr spread
 #' @export
-global_load_cl_landing_rdb_data <- function(input_path_cl_landing_rdb_data) {
+global_load_cl_landing_rdb_data <- function(input_path_file_rcg_stats) {
   cat(format(x = Sys.time(),
              format = "%Y-%m-%d %H:%M:%S"),
-      " - Start process for load data from CL landing RDBES data.\n",
+      " - Start process for load data from CL landings RDB data.\n",
       sep = "")
   # global arguments verifications ----
   # to do in the future
   # process ----
-  cl_landing_rdbes_data <- withCallingHandlers(expr = readr::read_csv(file = input_path_cl_landing_rdbes_data),
-                                                                      col_names = c("cl_commercial_landing_id",
-                                                                                    "landing_country",
-                                                                                    "flag_country",
-                                                                                    "year",
-                                                                                    "quarter",
-                                                                                    "month",
-                                                                                    "area",
-                                                                                    "fishing_ground",
-                                                                                    "region",
-                                                                                    "statistical_rectangle",
-                                                                                    "subpolygon",
-                                                                                    "species_aphia_id",
-                                                                                    "species",
-                                                                                    "species_desc",
-                                                                                    "stock",
-                                                                                    "landing_category",
-                                                                                    "size_category_scale",
-                                                                                    "size_category",
-                                                                                    "fishing_activity_category_national",
-                                                                                    "fishing_activity_category_european_level5",
-                                                                                    "fishing_activity_category_european_level6",
-                                                                                    "harbour",
-                                                                                    "harbour_desc",
-                                                                                    "vessel_length_category",
-                                                                                    "unallocated_catch_weight",
-                                                                                    "area_misreported_catch_weight",
-                                                                                    "official_landing_catch_weight",
-                                                                                    "landing_multiplier",
-                                                                                    "official_landing_value"),
-                                                                      col_types = "dccddccccccdccccccccccccnnnnn",
-                                                                      skip = 1,
-                                                                      na = c("",
-                                                                             "NA",
-                                                                             "NULL")),
+
+  # Define EU countries
+  EU27_2020 <- c("AUT",
+                 "BEL",
+                 "BGR",
+                 "HRV",
+                 "CYP",
+                 "CZE",
+                 "DNK",
+                 "EST",
+                 "FIN",
+                 "FRA",
+                 "DEU",
+                 "GRC",
+                 "HUN",
+                 "IRL",
+                 "ITA",
+                 "LVA",
+                 "LTU",
+                 "LUX",
+                 "MLT",
+                 "NLD",
+                 "POL",
+                 "PRT",
+                 "ROU",
+                 "SVK",
+                 "SVN",
+                 "ESP",
+                 "SWE")
+
+  cl_landing_rdb_data <- withCallingHandlers(expr = readr::read_csv(paste0(input_path_file_rcg_stats),
+                                                                    na = c("", "NA", "NULL")),
+
                                                warning = function(input_path_cl_landing_rdbes_data) {
                                                  stop("Troubles with the input file, check the format associated.\n")
                                                })
   cat(format(x = Sys.time(),
              format = "%Y-%m-%d %H:%M:%S"),
-      " - Successful process for load data from CL landing RDB data.\n",
+      " - Successful process for load data from CL landings RDB data.\n",
       sep = "")
-  return(cl_landing_rdbes_data)
+
+  # transform RDB data into the same format as EUROSTAT
+  #3 Add country codes from geo data file
+  geo_data <- utils::read.table(file = system.file("geo.def",
+                                                   package = "rwptool"),
+                                header = TRUE,
+                                sep = ";")
+
+  cl_landing_rdb_data_1 <- dplyr::left_join(cl_landing_rdb_data, geo_data,
+                                            by = c("FlagCountry" = "level_description"),
+                                            keep = T)
+
+
+  # Summarise and create a EU27_2020 line
+
+  cl_landing_rdb_data_ctry <-
+    dplyr::summarise(
+      dplyr::group_by(cl_landing_rdb_data_1, Year, Species, Area, geo),
+      TLW = sum(OfficialLandingCatchWeight / 1000, na.rm = T),
+      .groups = "drop"
+    )
+  cl_landing_rdb_data_eu27 <-
+    dplyr::summarise(
+      dplyr::group_by(
+        dplyr::filter(cl_landing_rdb_data_1, FlagCountry %in% !!EU27_2020) ,
+        Year,
+        Species,
+        Area
+      ),
+      geo = "EU27_2020",
+      TLW = sum(OfficialLandingCatchWeight / 1000, na.rm = T),
+      .groups = "drop"
+    )
+
+  # Include code for EU28 + warning if UK countries are missing
+
+  cl_landing_rdb_data_2 <- rbind(cl_landing_rdb_data_ctry, cl_landing_rdb_data_eu27)
+
+  cl_landing_rdb_data_t <- tidyr::spread(cl_landing_rdb_data_2, key = Year, value = TLW)
+  cl_landing_rdb_data_t <- dplyr::rename(cl_landing_rdb_data_t, Scientific_Name  = Species)
+
+
+
+  return(cl_landing_rdb_data)
 }
