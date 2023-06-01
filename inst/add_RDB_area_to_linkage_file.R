71ac6d28dd8ab2d79d032930f46ad5eca784cc7c
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2023-06-01 09:08:52 +0200

    Fixed the area problem and claned up the code a bit

--- inst/add_RDB_area_to_linkage_file.R
+++ inst/add_RDB_area_to_linkage_file.R
@@ -12,7 +12,10 @@ library(dplyr)
 library(stringr)
 library(data.table)
 
-path <- "Q:/mynd/kibi/projects_wks_wgs_rcgs/ISSG_RWP/2022/table_2_1_development/rwptool/inst/"
+
+path <- "./inst/"
+linkage_file_name <- "eumap_table_2_1_linkage_version_2022_v1.0"
+suffix_out_file <- "_w_rdb_area"
 
 # Get area file from the RDB and keep only relevant info
 
@@ -21,13 +24,16 @@ names(rdb_areas)
 
 unique(rdb_areas$X_AreaTypeCode)
 
-rdb_areas <- subset(rdb_areas, X_AreaTypeCode != "Area" | Code == "27.5")
+rdb_areas <- subset(rdb_areas, X_AreaTypeCode != "Area" | Code == "27.5") # Area 27.5 has wrongly been coded as a area
 
+unique(rdb_areas$Code)
+
+# Create a list of areas
 rdb_areas <- unique(str_subset(rdb_areas$Code, "[:digit:]"))
 
 # Load linkage file
 
-linkage <- read.csv(file.path(path, "eumap_table_2_1_linkage_version_2022_v1.0.csv"), sep = ";", header = T)
+linkage <- read.csv(paste0(path, linkage_file_name, ".csv"), sep = ";", header = T)
 
 names(linkage)
 
@@ -39,7 +45,7 @@ linkage$area_bis <- gsub(" ", "", linkage$area_bis)
 
 
 # Coding RDB areas ----
-
+## Start with a copy of the EUROSTAT areas - to stop the loop
 linkage$area_rdb <- paste0(linkage$area_bis, ",")
 unique(linkage$area_rdb)
 
@@ -48,14 +54,27 @@ unique(linkage$area_rdb)
 
 # Replace areas on upper level with all underlying areas
 
-# rdb_areas <- c("27.3.d.28.2", "27.3.d.28", "27.3.d.28.1")
 
 for (i in rdb_areas) {
 
+
   j <- paste0(toupper(gsub("\\.", "\\_", i)), ",")
 
-  linkage$area_rdb <- gsub(j, paste0(paste(rdb_areas[rdb_areas %like% i], collapse = ","), ","), linkage$area_rdb)
+  k <- paste0(i, ".")
+
+  k <- gsub("\\.", "\\\\.", k)
+
+
+  linkage$area_rdb <- gsub(j, gsub(" ", "", paste(i, paste(str_subset(rdb_areas, k), collapse = ","), sep = ",")), linkage$area_rdb)
 
 }
 
+test <- distinct(linkage, area_bis, area_rdb)
+
+# Remove trailing comma
+
+linkage$area_rdb <- gsub(",$", "", linkage$area_rdb)
+
+
+write.table(linkage, paste0(path, linkage_file_name, suffix_out_file, ".csv"), sep = ";")
 
